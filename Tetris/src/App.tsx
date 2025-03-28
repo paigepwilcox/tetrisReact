92600e773d3273ea5d36785722b979756fa6566f
Paige <ph.paigee@gmail.com>
2025-03-28 14:36:52 -0500

    created the board and added basic styling

--- Tetris/src/App.tsx
+++ Tetris/src/App.tsx
@@ -1,11 +1,18 @@
+import Board from './components/Board';
+import { EmptyCell } from './components/types';
+import './components/types';
+
+const board = Array(20)
+  .fill(null)
+  .map(() => Array(12).fill(EmptyCell.Empty));
 
 function App() {
   return (
-    <>
-      <div>
+      <div className='App'>
+        <h1>Tetris!</h1>
+        <Board currentBoard={board}/>
       </div>
-    </>
-  )
+  );
 }
 
 export default App
