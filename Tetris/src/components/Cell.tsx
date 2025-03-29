d82ee0e03e2eb92b966f543723f5923e0bc6859d
Paige <ph.paigee@gmail.com>
2025-03-29 11:14:06 -0500

    added a Cell component for the board

---
+++ Tetris/src/components/Cell.tsx
@@ -0,0 +1,11 @@
+import { CellOptions } from "./types";
+
+interface Props {
+    type: CellOptions;
+}
+
+function Cell({ type }: Props) {
+    return <div className={`cell ${type}`}></div>;
+}
+
+export default Cell;
