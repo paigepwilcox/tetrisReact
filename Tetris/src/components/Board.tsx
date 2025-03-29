d2c805cd4795b1b5545faf827f588c99de09b36a
Paige <ph.paigee@gmail.com>
2025-03-29 11:13:11 -0500

    Board component

---
+++ Tetris/src/components/Board.tsx
@@ -0,0 +1,23 @@
+import Cell from './Cell';
+import { BoardShape } from './types';
+
+interface Props {
+    currentBoard: BoardShape;
+}
+
+function Board({ currentBoard }: Props) {
+    return (
+        <div className="board">
+            {currentBoard.map((row, rowIndex) => (
+                <div className="row" key={ `${rowIndex}` }>
+                    {row.map((cell, colIndex) => (
+                        <Cell key={`${rowIndex} - ${colIndex}`} type={cell}></Cell>
+                    ))}
+                </div>
+            ))}
+        </div>
+    );
+
+}
+
+export default Board;
