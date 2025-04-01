30ba7fcea8b6b8d021bab7d08f8a5c34f64f3fc2
Paige <ph.paigee@gmail.com>
2025-04-01 13:14:09 -0500

    added score

--- Tetris/src/App.tsx
+++ Tetris/src/App.tsx
@@ -3,13 +3,14 @@ import { useTetris } from './hooks/useTetris';
 import './types';
 
 function App() {
-  const { board, startGame, isPlaying} = useTetris();
+  const { board, startGame, isPlaying, score } = useTetris();
 
   return (
       <div className='App'>
         <h1>Tetris!</h1>
         <Board currentBoard={board}/>
         <div className='controls'>
+          <h2>Score: {score}</h2>
           {isPlaying ? null : (
             <button onClick={startGame}>New Game</button>
           )}
