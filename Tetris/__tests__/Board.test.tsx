baa391a2f628f9843a1bc13be817011e4074b10b
Paige <ph.paigee@gmail.com>
2025-06-09 15:25:06 -0500

    cleaned up unused code

--- Tetris/__tests__/Board.test.tsx
+++ Tetris/__tests__/Board.test.tsx
@@ -1,14 +1,4 @@
-import { render } from "@testing-library/react";
-import React from "react";
-import Board from '../src/components/Board';
 import { getEmptyBoard } from '../src/hooks/useTetrisBoard';
-
-/*
-1. Board rendered correct amount of spaces 
-boardshape = num of 
-
-2. Board 
-*/
 
 describe('Board', () => {
     it("Board rendered the correct amount of cells or spaces", () => {
